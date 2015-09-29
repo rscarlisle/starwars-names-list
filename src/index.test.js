05871e2c15ae4eef07d23d3f425602fafbc7b399
Randy Carlisle <randy@Admins-MacBook-Pro-99.local>
2015-09-28 19:27:22 -0700

    chore(releasing): Add Travis config conventional commit and semantic-release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starWars-names', function() {
+    it('should work!', function() {
+        expect(true).to.be.true;
+    });
+});
