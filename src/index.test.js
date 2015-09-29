6a2694ad647079e98db85fbfce7afa0ecd4bc74d
Randy Carlisle <randy@Admins-MacBook-Pro-99.local>
2015-09-29 00:08:04 -0700

    feat(random): add ability to get an array of star wars names
    if you pass a number to the random function, you will receive an array with that numberof
    
    closes #2

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,34 @@ var expect = require('chai').expect;
 var starWars = require('./index');
 
 describe('starWars-names', function() {
-    it('should work!', function() {
-        expect(true).to.be.true;
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      }
     });
+
+    it('should contain "Luke Skywalker"', function() {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from the starWars.all', function () {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+
+    it('should return an array of random items if passed', function () {
+      var randomItems = starWars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(starWars.all).to.include(item);
+      });
+    });
+  });
 });
